68c69919690c32261378d7b9e34ef20bb211436c
D-Berg <danielfjberg@gmail.com>
2023-08-11 15:49:52 +0200

    day 08 p2: complete

--- day_08_treetop_tree_house/src/main.rs
+++ day_08_treetop_tree_house/src/main.rs
@@ -30,18 +30,12 @@ fn main() -> Result<(), Box<dyn Error + 'static>>{
 
     // Compute ---------------------------------------------------------------
 
-    let mut n_visible_trees: i32 = 0;
+    let mut scenic_scores: Vec<i32> = Vec::new();
     
     // add edges since all of them are visible
     let rows: usize = tree_map.len();
     let cols: usize = tree_map[0].len();
 
-    n_visible_trees += 2 * rows as i32; // add top and bottow edges
-    n_visible_trees += 2 * (cols - 2) as i32; // add side edges
-
-    dbg!(&n_visible_trees);
-    // TODO: compute which interior trees are visible
-    // check interior
 
     // traversing trees is O(MN)
     // checking if tree is visible is O(NM)?
@@ -49,95 +43,77 @@ fn main() -> Result<(), Box<dyn Error + 'static>>{
 
         for j in 1..(cols - 1) {
 
-
-            let mut tree_is_visible_from_above: bool = true;
-            let mut tree_is_visible_from_below: bool = true;
-
-            let mut tree_is_visible_from_right: bool = true;
-            let mut tree_is_visible_from_left: bool = true;
-
             let current_tree = tree_map[i][j];
 
+            let mut scenic_score = 1;
+            let mut viewing_distance = 0;
             // check that all trees above are shorter than current tree
 
-            dbg!(&current_tree);
-            
-            'above: for k in 0..i {
+            'above: for k in (0..i).rev() {
 
                 // if a tree above is equal or taller than
                 // current tree is not visible from above.
-
-                dbg!(tree_map[k][j]);
-
+                viewing_distance += 1;
                 if tree_map[k][j] >= current_tree {
-                    tree_is_visible_from_above = false;
                     break 'above;
                 }
                 
             }
-
-            dbg!(&tree_is_visible_from_above);
-
-            if tree_is_visible_from_above {
-                n_visible_trees += 1;
-                // skip other checks.
-                continue;
+            
+            if viewing_distance != 0 {
+                scenic_score *= viewing_distance;
             }
 
-            
+            viewing_distance = 0;
             // check treees below 
 
             'below: for k in (i + 1)..rows {
+                viewing_distance += 1;
                 if tree_map[k][j] >= current_tree {
-                    tree_is_visible_from_below = false;
                     break 'below;
                 }
+
             }
 
-            if tree_is_visible_from_below {
-                n_visible_trees += 1;
-                continue;
+            if viewing_distance != 0 {
+                scenic_score *= viewing_distance;
             }
+            viewing_distance = 0;
 
             // check trees to the right
 
             'right: for l in (j + 1)..cols {
+                viewing_distance += 1;
                 if tree_map[i][l] >= current_tree {
-                    tree_is_visible_from_right = false;
                     break 'right;
                 }
             }
 
-            if tree_is_visible_from_right {
-                n_visible_trees += 1;
-                continue;
+            if viewing_distance != 0 {
+                scenic_score *= viewing_distance;
             }
+            viewing_distance = 0;
             // check trees from the left
-
-            'left: for l in 0..j {
+            'left: for l in (0..j).rev() {
                 
+                viewing_distance += 1;
                 if tree_map[i][l] >= current_tree {
-                    tree_is_visible_from_left = false;
                     break 'left;
                 }
+
             }
 
-            if tree_is_visible_from_left {
-                n_visible_trees += 1;
-                continue;
+            if viewing_distance != 0 {
+                scenic_score *= viewing_distance;
             }
-
+            scenic_scores.push(scenic_score);
 
         }
     }
 
 
-    println!("Number of visible trees: {}", n_visible_trees);
-
-    let mut test_vec: Vec<Vec<i32>> = Vec::new();
-    test_vec.push(Vec::new());
-    test_vec[0].push(1);
-    dbg!(test_vec[0][0]);
+    scenic_scores.sort();
+    dbg!(scenic_scores);
 
     Ok(())
 }
